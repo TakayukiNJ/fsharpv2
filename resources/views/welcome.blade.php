808b77dc10b88f537ef677f8e823a19967f8af2e
[TakayukiNJ] <[nj.takayuki@gmail.com]>
2019-10-16 19:27:08 +0900

    https://www.cloudways.com/blog/laravel-paypal-payment-gateway/ こちらを参考に実装してるけど、エラー出てるな

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,11 +95,11 @@
                 {{--</form>--}}
                 <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" name="frmTransaction" id="frmTransaction">
                     @csrf
-                    <input type="hidden" name="business" value="{{$paypal_id}}">
+                    <input type="hidden" name="business" value="{{ $paypal_id ?? '' }}">
                     <input type="hidden" name="cmd" value="_xclick">
-                    <input type="hidden" name="item_name" value="{{$product->name}}">
-                    <input type="hidden" name="item_number" value="{{$product->id}}">
-                    <input type="hidden" name="amount" value="{{$product->price}}">
+                    <input type="hidden" name="item_name" value="{{ $product ?? '' }}">
+                    {{--<input type="hidden" name="item_number" value="{{$product->id}}">--}}
+                    {{--<input type="hidden" name="amount" value="{{$product->price}}">--}}
                     <input type="hidden" name="currency_code" value="USD">
                     <input type="hidden" name="cancel_return" value="http://demo.expertphp.in/payment-cancel">
                     <input type="hidden" name="return" value="http://demo.expertphp.in/payment-status">
